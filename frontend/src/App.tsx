b66f48c91d4cc5ed6e6a98eab1e285cd79d36dca
Ritojnan <ritojnanm@gmail.com>
2025-02-01 13:42:59 +0530

    frontend initialized

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { Route, Routes } from "react-router-dom";
+
+import Landing from "./pages/landing";
+
+function App() {
+   return (
+      <Routes>
+         <Route path="/" element={<Landing />} />
+      </Routes>
+   );
+}
+
+export default App;
